975c164f988616b79d66ddfc48231dff84138a4d
gh-nuvvuru-vishal-ram-reddy <nuvvuru.vishal.ram.reddy@ltim.trumio.ai>
2025-07-22 14:20:48 +0530

    add logic for user registartion and login UserRepository.java and AuthService are updated

--- loanapp_backend/src/main/java/com/lti/loanapp/repository/UserRepository.java
+++ loanapp_backend/src/main/java/com/lti/loanapp/repository/UserRepository.java
@@ -1,5 +1,13 @@
 package com.lti.loanapp.repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.lti.loanapp.entity.User;
 
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> 
+{
+
+    User findByUsernameAndPassword(String username, String password);
+    // Custom query methods can be defined here if needed
 }
